e4e4c3528c07fe50885a133cb6626f5de2487eca
QwistyS <onecoolterran@gmail.com>
2024-08-15 10:49:50 +0300

    Avl supports cuncorency platform independet.
    *Not testetd yet.

--- inc/qwistys_avltree.h
+++ inc/qwistys_avltree.h
@@ -18,6 +18,23 @@ typedef struct avlt_node_t {
     struct avlt_node_t *parent;
 } avlt_node_t;
 
+// Thread-safe related declarations
+typedef struct qwistys_mutex_t qwistys_mutex_t;
+
+typedef void (*qwistys_mutex_init_fn)(qwistys_mutex_t *mutex);
+typedef void (*qwistys_mutex_destroy_fn)(qwistys_mutex_t *mutex);
+typedef void (*qwistys_mutex_lock_fn)(qwistys_mutex_t *mutex);
+typedef void (*qwistys_mutex_unlock_fn)(qwistys_mutex_t *mutex);
+
+typedef struct {
+    avlt_node_t *root;
+    qwistys_mutex_t *mutex;
+    qwistys_mutex_init_fn mutex_init;
+    qwistys_mutex_destroy_fn mutex_destroy;
+    qwistys_mutex_lock_fn mutex_lock;
+    qwistys_mutex_unlock_fn mutex_unlock;
+} avl_tree_t;
+
 // Function prototypes
 avlt_node_t *avlt_create_node(size_t user_data_length_in_bytes);
 uint32_t avlt_get_height(avlt_node_t *node);
@@ -31,9 +48,56 @@ void avlt_pre_order(avlt_node_t *root, void (*process_node)(void *));
 void avlt_in_order(avlt_node_t *root, void (*process_node)(void *));
 void avlt_post_order(avlt_node_t *root, void (*process_node)(void *));
 void avlt_print(avlt_node_t *root, void (*print_node)(void *));
+void avlt_free_tree(avlt_node_t *root, void (*del_data)(void *));
+
+// Function prototypes for thread-safe operations
+void avl_tree_init(avl_tree_t *tree, qwistys_mutex_t *mutex,
+                   qwistys_mutex_init_fn init_fn,
+                   qwistys_mutex_destroy_fn destroy_fn,
+                   qwistys_mutex_lock_fn lock_fn,
+                   qwistys_mutex_unlock_fn unlock_fn);
+avlt_node_t *avl_tree_insert(avl_tree_t *tree, void *user_data, size_t data_length, int (*cmp)(void *, void *));
+avlt_node_t *avl_tree_delete(avl_tree_t *tree, void *user_data, int (*cmp)(void *, void *), void (*del_data)(void *));
+void avl_tree_free(avl_tree_t *tree, void (*del_data)(void *));
 
 // Implementation section
 #ifdef QWISTYS_AVLT_IMPLEMENTATION
+
+void avl_tree_init(avl_tree_t *tree, qwistys_mutex_t *mutex,
+                   qwistys_mutex_init_fn init_fn,
+                   qwistys_mutex_destroy_fn destroy_fn,
+                   qwistys_mutex_lock_fn lock_fn,
+                   qwistys_mutex_unlock_fn unlock_fn) {
+    tree->root = NULL;
+    tree->mutex = mutex;
+    tree->mutex_init = init_fn;
+    tree->mutex_destroy = destroy_fn;
+    tree->mutex_lock = lock_fn;
+    tree->mutex_unlock = unlock_fn;
+    tree->mutex_init(tree->mutex);
+}
+
+avlt_node_t *avl_tree_insert(avl_tree_t *tree, void *user_data, size_t data_length, int (*cmp)(void *, void *)) {
+    tree->mutex_lock(tree->mutex);
+    tree->root = avlt_insert(tree->root, user_data, data_length, cmp);
+    tree->mutex_unlock(tree->mutex);
+    return tree->root;
+}
+
+avlt_node_t *avl_tree_delete(avl_tree_t *tree, void *user_data, int (*cmp)(void *, void *), void (*del_data)(void *)) {
+    tree->mutex_lock(tree->mutex);
+    tree->root = avlt_delete(tree->root, user_data, cmp, del_data);
+    tree->mutex_unlock(tree->mutex);
+    return tree->root;
+}
+
+void avl_tree_free(avl_tree_t *tree, void (*del_data)(void *)) {
+    tree->mutex_lock(tree->mutex);
+    avlt_free_tree(tree->root, del_data);
+    tree->root = NULL;
+    tree->mutex_unlock(tree->mutex);
+    tree->mutex_destroy(tree->mutex);
+}
 
 // Function to create a new node with user data
 avlt_node_t *avlt_create_node(size_t user_data_length_in_bytes) {
